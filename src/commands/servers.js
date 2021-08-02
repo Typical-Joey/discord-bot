424f3b27d630120a7c9f3e7143c96fc4fb733762
Typical-Joey <jtcapocci@yahoo.com>
2021-08-02 16:51:39 -0500

    Add server ips

--- src/commands/servers.js
+++ src/commands/servers.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const Discord = require('discord.js');
 
 const serverList = new Discord.MessageEmbed()
   .setColor('#0099ff')
   .setTitle('Server IP\'s')
   .addFields(
-    { name: 'Minecraft', value: '2.2.2.2' },
-    { name: 'Rust', value: '1.1.1.1' },
+    { name: 'Minecraft', value: process.env.MINECRAFTIP },
+    { name: 'Rust', value: process.env.RUSTIP },
   );
 
 module.exports = function(msg) {
