d2ba571e6c629648153ec8881875d47dcb8865c4
Typical-Joey <jtcapocci@yahoo.com>
2021-08-10 18:17:19 -0500

    Took out channel validation

--- src/commandHandler.js
+++ src/commandHandler.js
@@ -14,17 +14,19 @@ const commands = {
 module.exports = async function commandHandler(msg) {
   const prefix = '!';
   let token = msg.content;
+
   // Validates the correct channel id
-  if (msg.channel.id === process.env.CHANNELID) {
-    // Validates a command was attempted
-    if (token[0] === prefix) {
-      token = token.substring(1).split(' ');
-      const command = token.shift();
-      // Validates command
-      if(commands[command]) {
-        const args = token;
-        commands[command](msg, args);
-      }
+  // if (msg.channel.id === process.env.CHANNELID) {
+  // }
+  // Validates a command was attempted
+
+  if (token[0] === prefix) {
+    token = token.substring(1).split(' ');
+    const command = token.shift();
+    // Validates command
+    if(commands[command]) {
+      const args = token;
+      commands[command](msg, args);
     }
   }
 };
